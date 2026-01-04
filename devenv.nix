1733a88f439c91753a1b11d5e327686f3fdd1afc
Jeremy Hauschildt <jeremy@jhauschildt.com>
2026-01-03 16:00:39 -0800

    chore: Improve workflows, development environment, cleanup (#42)
    Workflow updates:
    - Rename workflows
    - Update workflows to have very large GC limit to avoid any GC (devenv
    output GC roots are always removed on GC, and take a majority of the
    time)
    - General cleanup
    - Update dependabot so that updates are grouped
    
    Devenv:
    - Refactor/rename/reorder some stuff in devenv.nix
    - Move some more stuff to be conditionally enabled
    - Add a hacky script replacement for `oic-fox-fuckery-cli` that only
    builds the derivation at runtime. This avoids building it every rebuild,
    which is not needed during development and is very expensive, and allows
    it to be only conditionally included in `packages`.
    - bump devenv.yaml, why not...
    - Various cleanup

--- devenv.nix
+++ devenv.nix
@@ -5,11 +5,11 @@
   lib,
   ...
 }: let
-  oic-fox-fuckery = config.languages.rust.import ./. {};
   project_name = "oic-fox-fuckery";
+  oic_fox_fuckery_cli = config.languages.rust.import ./. {};
   registry_user = "doot";
   tag = "latest";
-  oic-fox-fuckery-prod-deriv = inputs.nix2container.packages.x86_64-linux.nix2container.buildImage {
+  prod_image = inputs.nix2container.packages.x86_64-linux.nix2container.buildImage {
     # Use nix2container directly since devenv containers include the entire environment, which is several GBs. This way the container is < 100 MB
     name = "${registry_user}/${project_name}";
     inherit tag;
@@ -18,43 +18,52 @@
       ExposedPorts = {
         "5150/tcp" = {};
       };
-      EntryPoint = ["bin/oic-fox-fuckery-cli"];
+      EntryPoint = ["bin/${project_name}-cli"];
     };
     copyToRoot = pkgs.buildEnv {
       name = "image-root";
-      paths = [oic-fox-fuckery ./. ./config];
+      paths = [oic_fox_fuckery_cli ./. ./config];
       pathsToLink = ["/bin" "/config"];
     };
   };
 in {
   name = project_name;
 
+  outputs = {
+    inherit oic_fox_fuckery_cli;
+    prod_image_copy_local = prod_image.copyToDockerDaemon;
+    prod_image_copy_registry = prod_image.copyToRegistry;
+  };
+
   cachix.pull = ["pre-commit-hooks"];
 
+  devcontainer.enable = true;
+
+  delta.enable = true;
+
   languages = {
-    nix.enable = !config.container.isBuilding;
+    nix.enable = !config.container.isBuilding && !config.devenv.isTesting;
     rust = {
       enable = true;
       toolchainFile = ./rust-toolchain.toml;
       rustflags = "-Z threads=8";
       mold.enable = true;
-      components = [
-        "rustc"
-        "cargo"
-        "clippy"
-        "rustfmt"
-        "rust-analyzer"
-        "rustc-codegen-cranelift-preview"
-      ];
     };
   };
-  # TODO: there is an unhandled error when the api key is bad...
 
-  outputs = {
-    inherit oic-fox-fuckery;
-    oic-fox-fuckery-prod-local = oic-fox-fuckery-prod-deriv.copyToDockerDaemon;
-    oic-fox-fuckery-prod-push = oic-fox-fuckery-prod-deriv.copyToRegistry;
-  };
+  packages =
+    lib.optionals (!config.container.isBuilding && !config.devenv.isTesting) [
+      # Development packages to include only when not building a container or testing
+      pkgs.bacon
+      pkgs.atop
+      pkgs.loco
+      pkgs.statix
+      pkgs.deadnix
+      pkgs.nil
+    ]
+    ++ lib.optionals config.container.isBuilding [
+      oic_fox_fuckery_cli # Project package
+    ];
 
   tasks = {
     "container:local" = {
@@ -63,7 +72,7 @@ in {
 
         echo "Building docker image and copying it to local docker daemon..."
 
-        copyscript=$(devenv build outputs.oic-fox-fuckery-prod-local)
+        copyscript=$(devenv build outputs.prod_image_copy_local)
 
         echo "Loading image into docker daemon via $copyscript..."
         $copyscript/bin/copy-to-docker-daemon
@@ -76,6 +85,7 @@ in {
         "config/**/*.yaml"
         "*.toml"
         "devenv.nix"
+        "*.lock"
       ];
     };
 
@@ -85,56 +95,35 @@ in {
 
         echo "Building docker image and copying it to remote registry..."
 
-        copyscript=$(devenv build outputs.oic-fox-fuckery-prod-push)
+        copyscript=$(devenv build outputs.prod_image_copy_registry)
 
-        echo "Loading image into docker daemon via $copyscript..."
+        echo "Pushing image to registry via $copyscript..."
         $copyscript/bin/copy-to-registry --dest-creds ${registry_user}:$REGISTRY_API_KEY
 
-        echo "Copied container into remote registry: ${registry_user}/${project_name}:${tag}"
+        echo "Pushed container into remote registry: ${registry_user}/${project_name}:${tag}"
         echo '{ "image": "${registry_user}/${project_name}:${tag}" }' > $DEVENV_TASK_OUTPUT_FILE
       '';
       execIfModified = [
         "src/**/*.rs"
-        "config/**/*.yaml"
         "*.toml"
+        "config/**/*.yaml"
         "devenv.nix"
+        "*.lock"
       ];
     };
   };
-
-  packages =
-    lib.optionals (!config.container.isBuilding && !config.devenv.isTesting) [
-      # Development packages to include only when not building a container or testing
-      pkgs.git
-      pkgs.bacon
-      pkgs.atop
-      pkgs.loco
-      pkgs.statix
-      pkgs.deadnix
-      pkgs.nil
-      # pkgs.sccache
-    ]
-    ++ lib.optionals config.container.isBuilding [
-      oic-fox-fuckery # Project package
-    ];
-
-  # https://devenv.sh/processes/
-  # processes = {
-  #   watch.exec = "bacon run";
-  #   clippy.exec = "bacon clippy-all";
-  # };
 
   containers = {
     # TODO: This container still includes the entire dev environment, making it 3-4 GB. It should not be used until there is a way to only include the rust
     # binary. Use the nix2container output above instead.
     prod = {
-      name = "oic-fox-fuckery";
-      entrypoint = ["bin/oic-fox-fuckery-cli"];
+      name = project_name;
+      entrypoint = ["bin/${project_name}-cli"];
       startupCommand = "start";
       copyToRoot = pkgs.buildEnv {
         name = "image-root";
         paths = [
-          oic-fox-fuckery
+          oic_fox_fuckery_cli
           ./config
           ./.
           # TODO: This puts every file in config dir into the root and there does not appear to be a way to get the config dir itself
@@ -144,14 +133,16 @@ in {
     };
   };
 
-  delta.enable = true;
+  # Hacky way to avoid putting CLI in 'packages' and delay building the CLI until needed
+  scripts."${project_name}-cli".exec = ''
+    $(devenv build -q outputs.oic_fox_fuckery_cli)/bin/${project_name}-cli "$@"
+  '';
 
-  # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     cargo fmt --check
-    cargo clippy --all-targets --all-features
     cargo build
+    cargo clippy --all-targets --all-features
     cargo test
   '';
 
@@ -163,39 +154,35 @@ in {
 
   git-hooks = {
     hooks = {
-      commitizen.enable = true;
-      deadnix.enable = true;
-      statix.enable = true;
-      alejandra.enable = true;
+      commitizen.enable = !config.container.isBuilding;
+      deadnix.enable = !config.container.isBuilding;
+      statix.enable = !config.container.isBuilding;
+      alejandra.enable = !config.container.isBuilding;
       markdownlint = {
-        enable = true;
+        enable = !config.container.isBuilding;
         settings.configuration = {
           MD013 = {
             line_length = 180;
           };
         };
       };
-      check-json.enable = true;
+      check-json.enable = !config.container.isBuilding;
       pretty-format-json = {
-        enable = true;
+        enable = !config.container.isBuilding;
         args = ["--no-sort-keys"];
       };
-      cargo-check.enable = true;
+      cargo-check.enable = !config.container.isBuilding;
       clippy = {
         enable = true;
         settings.allFeatures = true;
         settings.denyWarnings = true;
       };
       rustfmt = {
-        enable = true;
+        enable = !config.container.isBuilding;
         settings.config-path = ".rustfmt.toml";
       };
-      check-toml.enable = true;
-      check-yaml.enable = true;
+      check-toml.enable = !config.container.isBuilding;
+      check-yaml.enable = !config.container.isBuilding;
     };
   };
-
-  devcontainer.enable = true;
-
-  # env.RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
 }
